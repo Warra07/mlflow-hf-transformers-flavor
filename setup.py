6110abf76a44241ef9ee9ed77cd20ee32a81cd13
Warra07 <wacim29@hotmail.com>
2022-03-02 12:25:37 +0100

    modification str_import_module

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='mlflow_HFtransformers_flavor',
+setup(name='mlflow_hf_transformers',
       version='0.3',
       description='Mlflow flavors for pytorch huggingface transformers models',
       keywords='mlflow huggingface transformers',
