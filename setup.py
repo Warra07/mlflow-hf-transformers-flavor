b764e70e6fd69126b24c200d22da75a072e421db
Warra07 <wacim29@hotmail.com>
2022-03-02 11:37:06 +0100

    initial changes commit

--- setup.py
+++ setup.py
@@ -1,36 +1,11 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
 
-
-APP_NAME = "mlflow_vismod"
-VERSION = "1.1.1"
-LICENSE = "MIT"
-AUTHOR = "James Hibbard, Andrew Bauman"
-DESCRIPTION = "MLflow model flavour for mlflow_vismod visualization grammar"
-
-setup(
-    name=APP_NAME,
-    version=VERSION,
-    license=LICENSE,
-    author=AUTHOR,
-    description=DESCRIPTION,
-    install_requires=[
-        "mlflow>=1.11.0",
-        "PyYAML>=5.3.1",
-        "altair>=4.1.0",
-        "altair_viewer>=0.3.0",
-    ],
-    extras_require={},
-    packages=find_namespace_packages("src"),
-    package_dir={"": "src"},
-    package_data={
-        "": ["*.yaml"],
-    },
-    entry_points="""
-    [console_scripts]
-    """,
-    python_requires=">=3.7",
-    classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
-    ],
-)
+setup(name='mlflow_HFtransformers_flavor',
+      version='0.3',
+      description='Mlflow flavors for pytorch huggingface transformers models',
+      keywords='mlflow huggingface transformers',
+      url='https://github.com/Warra07/mlflow-hf-transformers-flavor',
+      author='Wacim Belahcel',
+      author_email='wacimbelahcel@gmail.com',
+      license='Apache 2',
+      packages=['mlflow_hf_transformers'])
