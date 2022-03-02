e48a50488453016bc5bd723b6bb75146a83bec6f
Warra07 <wacim29@hotmail.com>
2022-03-02 15:00:02 +0100

    modification setup

--- mlflow_hf_transformers/__init__.py
+++ mlflow_hf_transformers/__init__.py
@@ -125,6 +125,7 @@ def get_default_conda_env(style):
 def log_model(
     model,
     artifact_path,
+    tokenizer=None,
     conda_env=None,
     code_paths=None,
     registered_model_name=None,
@@ -137,42 +138,19 @@ def log_model(
     extra_pip_requirements=None,
 ):
     """
-    Log a PyTorch model as an MLflow artifact for the current run.
+    Log a transformers model as an MLflow artifact for the current run.
 
-        .. warning::
 
-            Log the model with a signature to avoid inference errors.
-            If the model is logged without a signature, the MLflow Model Server relies on the
-            default inferred data type from NumPy. However, PyTorch often expects different
-            defaults, particularly when parsing floats. You must include the signature to ensure
-            that the model is logged with the correct data type so that the MLflow model server
-            can correctly provide valid input.
 
-    :param pytorch_model: PyTorch model to be saved. Can be either an eager model (subclass of
-                          ``torch.nn.Module``) or scripted model prepared via ``torch.jit.script``
-                          or ``torch.jit.trace``.
-
-                          The model accept a single ``torch.FloatTensor`` as
-                          input and produce a single output tensor.
-
-                          If saving an eager model, any code dependencies of the
-                          model's class, including the class definition itself, should be
-                          included in one of the following locations:
-
-                          - The package(s) listed in the model's Conda environment, specified
-                            by the ``conda_env`` parameter.
-                          - One or more of the files specified by the ``code_paths`` parameter.
+    :param model: transformers model to be saved. 
 
     :param artifact_path: Run-relative artifact path.
     :param conda_env: {{ conda_env }}
     :param code_paths: A list of local filesystem paths to Python file dependencies (or directories
                        containing file dependencies). These files are *prepended* to the system
                        path when the model is loaded.
-    :param pickle_module: The module that PyTorch should use to serialize ("pickle") the specified
-                          ``pytorch_model``. This is passed as the ``pickle_module`` parameter
-                          to ``torch.save()``. By default, this module is also used to
-                          deserialize ("unpickle") the PyTorch model at load time.
-    :param registered_model_name: If given, create a model version under
+
+        :param registered_model_name: If given, create a model version under
                                   ``registered_model_name``, also creating a registered model if one
                                   with the given name does not exist.
 
@@ -183,12 +161,6 @@ def log_model(
                       column omitted) and valid model output (e.g. model predictions generated on
                       the training dataset), for example:
 
-                      .. code-block:: python
-
-                        from mlflow.models.signature import infer_signature
-                        train = df.drop_column("target_label")
-                        predictions = ... # compute model predictions
-                        signature = infer_signature(train, predictions)
     :param input_example: Input example provides one or several instances of valid
                           model input. The example can be used as a hint of what data to feed the
                           model. The given example can be a Pandas DataFrame where the given
@@ -209,12 +181,6 @@ def log_model(
         A string containing the path to requirements file. Remote URIs are resolved to absolute
         filesystem paths. For example, consider the following ``requirements_file`` string:
 
-        .. code-block:: python
-
-            requirements_file = "s3://my-bucket/path/to/my_file"
-
-        In this case, the ``"my_file"`` requirements file is downloaded from S3. If ``None``,
-        no requirements file is added to the model.
 
     :param extra_files: A list containing the paths to corresponding extra files. Remote URIs
                       are resolved to absolute filesystem paths.
@@ -228,81 +194,10 @@ def log_model(
                       If ``None``, no extra files are added to the model.
     :param pip_requirements: {{ pip_requirements }}
     :param extra_pip_requirements: {{ extra_pip_requirements }}
-    :param kwargs: kwargs to pass to ``torch.save`` method.
     :return: A :py:class:`ModelInfo <mlflow.models.model.ModelInfo>` instance that contains the
              metadata of the logged model.
 
-    .. code-block:: python
-        :caption: Example
 
-        import numpy as np
-        import torch
-        import mlflow.pytorch
-
-        class LinearNNModel(torch.nn.Module):
-            def __init__(self):
-                super(LinearNNModel, self).__init__()
-                self.linear = torch.nn.Linear(1, 1)  # One in and one out
-
-            def forward(self, x):
-                y_pred = self.linear(x)
-                return y_pred
-
-        def gen_data():
-            # Example linear model modified to use y = 2x
-            # from https://github.com/hunkim/PyTorchZeroToAll
-            # X training data, y labels
-            X = torch.arange(1.0, 25.0).view(-1, 1)
-            y = torch.from_numpy(np.array([x * 2 for x in X])).view(-1, 1)
-            return X, y
-
-        # Define model, loss, and optimizer
-        model = LinearNNModel()
-        criterion = torch.nn.MSELoss()
-        optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
-
-        # Training loop
-        epochs = 250
-        X, y = gen_data()
-        for epoch in range(epochs):
-            # Forward pass: Compute predicted y by passing X to the model
-            y_pred = model(X)
-
-            # Compute the loss
-            loss = criterion(y_pred, y)
-
-            # Zero gradients, perform a backward pass, and update the weights.
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        # Log the model
-        with mlflow.start_run() as run:
-            mlflow.pytorch.log_model(model, "model")
-
-            # convert to scripted model and log the model
-            scripted_pytorch_model = torch.jit.script(model)
-            mlflow.pytorch.log_model(scripted_pytorch_model, "scripted_model")
-
-        # Fetch the logged model artifacts
-        print("run_id: {}".format(run.info.run_id))
-        for artifact_path in ["model/data", "scripted_model/data"]:
-            artifacts = [f.path for f in MlflowClient().list_artifacts(run.info.run_id,
-                        artifact_path)]
-            print("artifacts: {}".format(artifacts))
-
-    .. code-block:: text
-        :caption: Output
-
-        run_id: 1a1ec9e413ce48e9abf9aec20efd6f71
-        artifacts: ['model/data/model.pth',
-                    'model/data/pickle_module_info.txt']
-        artifacts: ['scripted_model/data/model.pth',
-                    'scripted_model/data/pickle_module_info.txt']
-
-    .. figure:: ../_static/images/pytorch_logged_models.png
-
-        PyTorch logged models
     """
     import torch, transformers
 
@@ -310,6 +205,7 @@ def log_model(
         artifact_path=artifact_path,
         flavor=mlflow_hf_transformers,
         model=model,
+        tokenizer=tokenizer,
         conda_env=conda_env,
         code_paths=code_paths,
         registered_model_name=registered_model_name,
@@ -327,6 +223,7 @@ def log_model(
 def save_model(
     model,
     path,
+    tokenizer=None,
     conda_env=None,
     mlflow_model=None,
     code_paths=None,
@@ -337,7 +234,7 @@ def save_model(
     pip_requirements=None,
     extra_pip_requirements=None,
 ):
-    """Save a visual model to a local file or a run.
+    """Save a transformers model to a local file or a run with tokenizer (optional).
 
 
     """
@@ -376,8 +273,12 @@ def save_model(
 
     model_path = os.path.join(model_data_path, _MODEL_DIR_SUBPATH)
     
+    
     model.save_pretrained(model_path)
 
+    if tokenizer is not None:
+        tokenizer_path = os.path.join(model_data_path, _TOKENIZER_DIR_SUBPATH)
+        tokenizer.save_pretrained(tokenizer_path)
 
 
     torchserve_artifacts_config = {}
@@ -480,9 +381,9 @@ def save_model(
 
 
 
-def load_model(model_uri, dst_path=None, **kwargs):
+def load_model(model_uri, dst_path=None):
     """
-    Load a PyTorch model from a local file or a run.
+    Load a transformers model from a local file or a run.
 
     :param model_uri: The location, in URI format, of the MLflow model, for example:
 
@@ -500,43 +401,9 @@ def load_model(model_uri, dst_path=None, **kwargs):
                      This directory must already exist. If unspecified, a local output
                      path will be created.
 
-    :param kwargs: kwargs to pass to ``torch.load`` method.
-    :return: A PyTorch model.
+    :return: A transformers model.
 
-    .. code-block:: python
-        :caption: Example
 
-        import torch
-        import mlflow.pytorch
-
-        # Class defined here
-        class LinearNNModel(torch.nn.Module):
-            ...
-
-        # Initialize our model, criterion and optimizer
-        ...
-
-        # Training loop
-        ...
-
-        # Log the model
-        with mlflow.start_run() as run:
-            mlflow.pytorch.log_model(model, "model")
-
-        # Inference after loading the logged model
-        model_uri = "runs:/{}/model".format(run.info.run_id)
-        loaded_model = mlflow.pytorch.load_model(model_uri)
-        for x in [4.0, 6.0, 30.0]:
-            X = torch.Tensor([[x]])
-            y_pred = loaded_model(X)
-            print("predict X: {}, y_pred: {:.2f}".format(x, y_pred.data.item()))
-
-    .. code-block:: text
-        :caption: Output
-
-        predict X: 4.0, y_pred: 7.57
-        predict X: 6.0, y_pred: 11.64
-        predict X: 30.0, y_pred: 60.48
     """
     import torch, transformers
 
@@ -569,6 +436,7 @@ def load_model(model_uri, dst_path=None, **kwargs):
             hf_transformers_conf["transformers_version"],
             transformers.__version__,
         )     
+
     transformers_model_artifacts_path = os.path.join(local_model_path, hf_transformers_conf["model_data"])
     return _load_model(path=transformers_model_artifacts_path,  
     model_class_name=hf_transformers_conf["model_class_name"])
@@ -579,12 +447,16 @@ def load_model(model_uri, dst_path=None, **kwargs):
 def _load_model(path, model_class_name):
     """
     :param path: The path to a serialized PyTorch model.
-    :param kwargs: Additional kwargs to pass to the PyTorch ``torch.load`` function.
+    :param model_class_name: transformers model class name
     """
     import torch, transformers
+    from transformers import AutoTokenizer
 
+
+    tokenizer = None
 
     model_path = os.path.join(path, _MODEL_DIR_SUBPATH)
+    tokenizer_path = os.path.join(path, _TOKENIZER_DIR_SUBPATH)
 
     try:
      model_class = getattr(transformers, model_class_name)
@@ -598,8 +470,14 @@ def _load_model(path, model_class_name):
             ),
             error_code=RESOURCE_DOES_NOT_EXIST,
         ) from exc
-    
-    return model_class.from_pretrained(model_path)
+
+    model = model_class.from_pretrained(model_path)
+    if os.path.exists(tokenizer_path):
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+        return model, tokenizer
+    return model
+
+
 
 
 
@@ -610,33 +488,12 @@ def _load_model(path, model_class_name):
 
 def log_tokenizer(tokenizer, artifact_path):
     """
-    Log a state_dict as an MLflow artifact for the current run.
+    Log a tokenizer
 
-    .. warning::
-        This function just logs a state_dict as an artifact and doesn't generate
-        an :ref:`MLflow Model <models>`.
-
-    :param state_dict: state_dict to be saved.
+    
+    :param tokenizer: tokenizer to be saved.
     :param artifact_path: Run-relative artifact path.
-    :param kwargs: kwargs to pass to ``torch.save``.
 
-    .. code-block:: python
-        :caption: Example
-
-        # Log a model as a state_dict
-        with mlflow.start_run():
-            state_dict = model.state_dict()
-            mlflow.pytorch.log_state_dict(state_dict, artifact_path="model")
-
-        # Log a checkpoint as a state_dict
-        with mlflow.start_run():
-            state_dict = {
-                "model": model.state_dict(),
-                "optimizer": optimizer.state_dict(),
-                "epoch": epoch,
-                "loss": loss,
-            }
-            mlflow.pytorch.log_state_dict(state_dict, artifact_path="checkpoint")
     """
     import torch, transformers
     try:
@@ -660,11 +517,10 @@ def log_tokenizer(tokenizer, artifact_path):
 
 def save_tokenizer(tokenizer, path):
     """
-    Save a state_dict to a path on the local file system
+    Save a tokenizer to a path on the local file system
 
-    :param state_dict: state_dict to be saved.
+    :param tokenizer: state_dict to be saved.
     :param path: Local path where the state_dict is to be saved.
-    :param kwargs: kwargs to pass to ``torch.save``.
     """
     import torch, transformers
 
@@ -690,31 +546,20 @@ def save_tokenizer(tokenizer, path):
 
 def load_tokenizer(tokenizer_uri):
     """
-    Load a state_dict from a local file or a run.
+    Load a tokenizer from a local file or a run.
 
-    :param state_dict_uri: The location, in URI format, of the state_dict, for example:
+    :param stokenizer_uri: The location, in URI format, of the state_dict, for example:
 
-                    - ``/Users/me/path/to/local/state_dict``
-                    - ``relative/path/to/local/state_dict``
-                    - ``s3://my_bucket/path/to/state_dict``
-                    - ``runs:/<mlflow_run_id>/run-relative/path/to/state_dict``
+                    - ``/Users/me/path/to/local/tokenizer``
+                    - ``relative/path/to/local/tokenizer``
+                    - ``s3://my_bucket/path/to/tokenizer``
+                    - ``runs:/<mlflow_run_id>/run-relative/path/to/tokenizer``
 
                     For more information about supported URI schemes, see
                     `Referencing Artifacts <https://www.mlflow.org/docs/latest/concepts.html#
                     artifact-locations>`_.
+    :return: A tokenizer
 
-    :param kwargs: kwargs to pass to ``torch.load``.
-    :return: A state_dict
-
-    .. code-block:: python
-        :caption: Example
-
-        with mlflow.start_run():
-            artifact_path = "model"
-            mlflow.pytorch.log_state_dict(model.state_dict(), artifact_path)
-            state_dict_uri = mlflow.get_artifact_uri(artifact_path)
-
-        state_dict = mlflow.pytorch.load_state_dict(state_dict_uri)
     """
     import torch, transformers
     from transformers import AutoTokenizer
