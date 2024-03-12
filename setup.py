f54e4e2eccd0165fa55dfdba7e6433cc7c58a072
Wacim Belahcel <wacim29@hotmail.com>
2024-03-12 11:27:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='mlflow_hf_transformers',
-      version='0.5',
+      version='0.6',
       description='Mlflow flavors for pytorch huggingface transformers models',
           install_requires=[
-        "mlflow==2.2.2",
-        "transformers[torch]==4.36.2",
+        "mlflow>=2.11.1",
+        "transformers[torch]>=4.36.2",
     ],
       keywords='mlflow huggingface transformers',
       url='https://github.com/Warra07/mlflow-hf-transformers-flavor',
